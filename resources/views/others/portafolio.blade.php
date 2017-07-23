8f206ecd441c524d69902a4d400e5bc1784e20f2
Daniel Pena <daniel.pena@intec.edu.do>
2017-07-23 00:20:35 -0400

    Second

---
+++ resources/views/others/portafolio.blade.php
@@ -0,0 +1,91 @@
+<!-- PORTAFOLIO -->
+@extends('layouts.master')
+
+@section('content')
+<div class="myContent">
+	<!-- Header -->
+	<div class="page-header">
+		<h1>Portafolio</h1>
+	</div>
+
+	<!-- Views -->
+	<div class='pBox'>
+		<img class="pImage" src="images/example.png" alt="Example">
+		<p>Descripcion aqui..</p>
+		<button type="button" class="pButton">Detalles</button>
+	</div>
+	<!-- End -->
+
+	<!-- Views -->
+	<div class='pBox'>
+		<img class="pImage" src="images/example.png" alt="Example">
+		<p>Descripcion aqui..</p>
+		<button type="button" class="pButton">Detalles</button>
+	</div>
+	<!-- End -->
+
+	<!-- Views -->
+	<div class='pBox'>
+		<img class="pImage" src="images/example.png" alt="Example">
+		<p>Descripcion aqui..</p>
+		<button type="button" class="pButton">Detalles</button>
+	</div>
+	<!-- End -->
+
+	<!-- Views -->
+	<div class='pBox'>
+		<img class="pImage" src="images/example.png" alt="Example">
+		<p>Descripcion aqui..</p>
+		<button type="button" class="pButton">Detalles</button>
+	</div>
+	<!-- End -->
+
+	<!-- Views -->
+	<div class='pBox'>
+		<img class="pImage" src="images/example.png" alt="Example">
+		<p>Descripcion aqui..</p>
+		<button type="button" class="pButton">Detalles</button>
+	</div>
+	<!-- End -->
+
+	<!-- Views -->
+	<div class='pBox'>
+		<img class="pImage" src="images/example.png" alt="Example">
+		<p>Descripcion aqui..</p>
+		<button type="button" class="pButton">Detalles</button>
+	</div>
+	<!-- End -->
+
+	<!-- Views -->
+	<div class='pBox'>
+		<img class="pImage" src="images/example.png" alt="Example">
+		<p>Descripcion aqui..</p>
+		<button type="button" class="pButton">Detalles</button>
+	</div>
+	<!-- End -->
+
+	<!-- Views -->
+	<div class='pBox'>
+		<img class="pImage" src="images/example.png" alt="Example">
+		<p>Descripcion aqui..</p>
+		<button type="button" class="pButton">Detalles</button>
+	</div>
+	<!-- End -->
+
+	<!-- Views -->
+	<div class='pBox'>
+		<img class="pImage" src="images/example.png" alt="Example">
+		<p>Descripcion aqui..</p>
+		<button type="button" class="pButton">Detalles</button>
+	</div>
+	<!-- End -->
+
+	<!-- Views -->
+	<div class='pBox'>
+		<img class="pImage" src="images/example.png" alt="Example">
+		<p>Descripcion aqui..</p>
+		<button type="button" class="pButton">Detalles</button>
+	</div>
+	<!-- End -->
+</div>
+@endsection
