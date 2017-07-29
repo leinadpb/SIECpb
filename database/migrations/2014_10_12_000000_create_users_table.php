8b51d5d4fb869c187907f79610a11ec93c23385b
Daniel Pena <daniel.pena@intec.edu.do>
2017-07-28 23:34:35 -0400

    To heroku

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->integer('age');
+            $table->integer('age')->nullable();
             $table->string('career');
             $table->string('tagline', 120);
             $table->rememberToken();
